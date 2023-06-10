580801f5054bc4c6fb4800fd069e412d6c1aee1a
Benedykt Cieśliński <benedykt.cieslinski@gmail.com>
2023-06-10 17:25:43 +0100

    Cleaned up FuelSystem logic.

--- PhysicsSamples/Assets/13. Raycast Car/Fuel/FuelSystem.cs
+++ PhysicsSamples/Assets/13. Raycast Car/Fuel/FuelSystem.cs
@@ -8,8 +8,6 @@ namespace RaycastCar
     [BurstCompile]
     public partial struct FuelSystem: ISystem
     {
-
-
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
@@ -25,7 +23,7 @@ namespace RaycastCar
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            var activeVehicleQuery = SystemAPI.QueryBuilder().WithAll<ActiveVehicle, Vehicle>().Build();
+            var activeVehicleQuery = SystemAPI.QueryBuilder().WithAll<ActiveVehicle, Vehicle, VehicleSpeed, VehicleFuel>().Build();
 
             var activeVehicle = Entity.Null;
             if (activeVehicleQuery.CalculateEntityCount() == 1)
@@ -39,48 +37,42 @@ namespace RaycastCar
 
             // Check if active vehicle is moving
             bool driveEngaged = false;
-            if (SystemAPI.HasComponent<VehicleSpeed>(activeVehicle))
-            {
-                var vehicleSpeed = SystemAPI.GetComponent<VehicleSpeed>(activeVehicle);
-                driveEngaged = vehicleSpeed.DriveEngaged != 0;
-            }
+            var vehicleSpeed = SystemAPI.GetComponent<VehicleSpeed>(activeVehicle);
+            driveEngaged = vehicleSpeed.DriveEngaged != 0;
 
 
             float currentFuel;
-            if (SystemAPI.HasComponent<VehicleFuel>(activeVehicle) && driveEngaged)
+            if (driveEngaged)
             {
                 var vehicleFuel = SystemAPI.GetComponent<VehicleFuel>(activeVehicle);
                 currentFuel = vehicleFuel.CurrentFuel;
 
-                currentFuel -= vehicleFuel.FuelDecreaseRatio * Time.deltaTime;
+                currentFuel -= vehicleFuel.FuelDecreaseRatio * SystemAPI.Time.DeltaTime;
                 currentFuel = Mathf.Clamp(currentFuel, 0, vehicleFuel.MaxFuel);
 
                 if (currentFuel <= 0)
                 {
-                    // somehow disable the car
-                    if (SystemAPI.HasComponent<VehicleSpeed>(activeVehicle))
+                    var speed = SystemAPI.GetComponent<VehicleSpeed>(activeVehicle);
+                    float newDesiredSpeed = 0;
+
+                    if (speed.DesiredSpeed > 0)
                     {
-                        var speed = SystemAPI.GetComponent<VehicleSpeed>(activeVehicle);
-                        float newDesiredSpeed = 0;
-
-                        if (speed.DesiredSpeed > 0)
-                        {
-                            newDesiredSpeed = speed.DesiredSpeed - 0.2f;
-                        }else if(speed.DesiredSpeed <= 0)
-                        {
-                            newDesiredSpeed = 0;
-                        }
-
-                        SystemAPI.SetComponent<VehicleSpeed>(activeVehicle, new VehicleSpeed {
-                            TopSpeed = speed.TopSpeed,
-                            DesiredSpeed = newDesiredSpeed,
-                            Damping = speed.Damping,
-                            DriveEngaged = 0,
-                        });
+                        newDesiredSpeed = speed.DesiredSpeed - 0.2f;
+                    }else if(speed.DesiredSpeed <= 0)
+                    {
+                        newDesiredSpeed = 0;
                     }
+
+                    // Set new speed
+                    SystemAPI.SetComponent<VehicleSpeed>(activeVehicle, new VehicleSpeed {
+                        TopSpeed = speed.TopSpeed,
+                        DesiredSpeed = newDesiredSpeed,
+                        Damping = speed.Damping,
+                        DriveEngaged = 0,
+                    });
                 }
 
-
+                // Set new fuel
                 SystemAPI.SetComponent<VehicleFuel>(activeVehicle, new VehicleFuel
                 {
                     MaxFuel = vehicleFuel.MaxFuel,
